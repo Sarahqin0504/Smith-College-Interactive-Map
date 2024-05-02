bd4b7e23f521f1975808b735c96ac84de6f5981e
Sarahqin0504 <157939543+Sarahqin0504@users.noreply.github.com>
2024-05-02 02:59:07 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -68,6 +68,42 @@ public class Player extends Thing implements Contract{
         }
     }
 
+    public void elevator(int floorNumber) {
+        if (currentPlace == setup.getLibrary().getFloors().get("Ground Floor")||
+            currentPlace == setup.getLibrary().getFloors().get("First Floor") || 
+            currentPlace == setup.getLibrary().getFloors().get("Second Floor")||
+            currentPlace == setup.getLibrary().getFloors().get("Third Floor") ||
+            currentPlace == setup.getLibrary().getFloors().get("Fourth Floor")){
+                // Use switch statement to handle different floor numbers
+            switch (floorNumber) {
+                case 0:
+                    // I should use setCurrentPlace(), but I forgot about it
+                    currentPlace = setup.getLibrary().getFloors().get("Ground Floor");
+                    // System.out.println("You are now at the Ground Floor.");
+                    break;
+                case 1:
+                    currentPlace = setup.getLibrary().getFloors().get("First Floor");
+                    // System.out.println("You are now at the First Floor.");
+                    break;
+                case 2:
+                    currentPlace = setup.getLibrary().getFloors().get("Second Floor");
+                    // System.out.println("You are now at the Second Floor.");
+                    break;
+                case 3:
+                    currentPlace = setup.getLibrary().getFloors().get("Third Floor");
+                    // System.out.println("You are now at the Third Floor.");
+                    break;
+                case 4:
+                    currentPlace = setup.getLibrary().getFloors().get("Fourth Floor");
+                    // System.out.println("You are now at the Fourth Floor.");
+                default:
+                    // If the input doesn't match any floor number, inform the player
+                    System.out.println("Invalid floor number.");
+                    break;
+            }
+        }
+    }
+
     public String getCurrentPlaceDescription() {
         return currentPlace.getDescription();
     }
@@ -98,14 +134,36 @@ public class Player extends Thing implements Contract{
     
 
     public void examine(String itemName) {
-        // Check if the item exists in the current room's inventory
-        for (Thing item : currentPlace.getList()) {
-            if (item.getName().equals(itemName)) {
-                // If the item exists, print its description
+        // Get the list of items from the current place
+        List<Thing> itemList = getCurrentPlaceItems();
+
+        // Iterate over the list of items in the current place
+        for (Thing item : itemList) {
+            // Check if the item name matches the provided itemName
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                // If a match is found, print its description
                 System.out.println("You examined " + itemName + ": " + item.getDescription());
                 return; // Exit the method after printing the description
             }
         }
+        // If no matching item is found, print a message indicating that the item cannot be found
+        System.out.println("The " + itemName + " cannot be found.");
+    }
+
+    public String lookAround(Place currentPlace) {
+        // Get the list of items from the current place
+        List<Thing> itemList = currentPlace.getList();
+        
+        // Initialize a StringBuilder to construct the description
+        StringBuilder description = new StringBuilder();
+        
+        // Append the description of each item to the StringBuilder
+        for (Thing item : itemList) {
+            description.append("Item: ").append(item.getName()).append("\n");
+        }
+        
+        // Return the concatenated description as a string
+        return "\n" + currentPlace.getDescription() + "\n" + description.toString();
     }
 
     public void use(String item) {
