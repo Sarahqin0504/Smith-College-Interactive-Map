d225c2feefbce1802cc9326ef183fe8f894e743a
Sarah Qin <sqin14@smith.edu>
2024-05-02 12:05:52 -0400

    text

--- BotanicGarden.java
+++ BotanicGarden.java
@@ -72,7 +72,7 @@ class Exhibition2 extends Place{
 }
 class SucculentHouse extends Place {
     public SucculentHouse() {
-        super("SucculentHouse", "This is the Succulent House, the oldest section of Lyman, built in 1894. It holds xerophytes, or plants adapted to deserts and dry habitats, including New World cacti, Old World succulent euphorbias, and plants from families such as the Crassulaceae and Asphodelaceae.");
+        super("SucculentHouse", "Succulent House. This is the Succulent House, the oldest section of Lyman, built in 1894. It holds xerophytes, or plants adapted to deserts and dry habitats, including New World cacti, Old World succulent euphorbias, and plants from families such as the Crassulaceae and Asphodelaceae.");
         // Add items to the ground floor inventory
         addItem(new Thing("New World cacti", "New World Cacti refers to the cacti that are native to the Americas. Cacti are a family of plants known scientifically as Cactaceae. They are mostly found in regions that range from Patagonia in South America through to areas of western Canada. "));
         addItem(new Thing("Old World succulent euphorbias", "Old World Cacti refer to succulent plants that resemble cacti and are native to the Old World (Africa, Europe, and Asia). These are not true cacti but are often part of different succulent families such as Euphorbiaceae or Crassulaceae."));
@@ -83,7 +83,7 @@ class SucculentHouse extends Place {
 
 class ShowHouse extends Place {
     public ShowHouse() {
-        super("ShowHouse", "Welcome to the Show House, where the first flower shows were staged. Today, it contains plants with foliar and floral scents, including salvias, mints, and geraniums, used in culinary, medicinal, and perfumery applications.");
+        super("ShowHouse", "Show House. Welcome to the Show House, where the first flower shows were staged. Today, it contains plants with foliar and floral scents, including salvias, mints, and geraniums, used in culinary, medicinal, and perfumery applications.");
         addItem(new Thing("salvias", "Salvias generally prefer locations with full sun and well-draining soil. They are drought-tolerant once established, making them suitable for xeriscaping or gardens in drier climates. The care and maintenance of salvia plants will depend on the species; some may be annuals, while others are perennials or biennials."));
         addItem(new Thing("mints", "The mint genus comprises numerous species and hybrids known for their distinctively sharp, cool flavor and fragrance, which is attributed to the compound menthol found in their leaves. Mint plants are hardy and grow well in moist, well-drained soils, preferably in full to partial sunlight. "));
     }
@@ -91,7 +91,7 @@ class ShowHouse extends Place {
 
 class OutdoorGarden extends Place {
     public OutdoorGarden() {
-        super("Outdoor Garden", "You have enter the Systematics Garden. Students can observe and compare flower types and growth forms among the garden’s species, chosen for their botanical and evolutionary interest. As one of our oldest gardens, it not only holds Smith’s history of learning, but also the centuries-long quest of the botanical science community to better understand how plants became what they are today. ");
+        super("Outdoor Garden", "Outdoor Garden.You have enter the Systematics Garden. Students can observe and compare flower types and growth forms among the garden’s species, chosen for their botanical and evolutionary interest. As one of our oldest gardens, it not only holds Smith’s history of learning, but also the centuries-long quest of the botanical science community to better understand how plants became what they are today. ");
         addItem(new Thing("Book", "There is a linear algebra textbook on the bench. Maybe someone left it here."));
     }
 }
